7e9862eb90241f464c2571a4a3088b1f7512bbd8
Nicolas Guillaume <nicolas.f.guillaume@gmail.com>
2020-02-02 21:58:46 +0100

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -7,14 +7,6 @@ section .text
         @ global means that the object file will contain a link to every label declared 'global'
         global _start
 
-@ registers are  part of the processor that temporarly holds memory
-@ in the x86_64 architecture, registers hold 64 bits
-
-@ a system call is when the program requests a service from the kernel
-@ all syscalls have an ID associated with them, and take arguments
-@ rax (contains ID), rdi (contains arg1), rsi (arg2), rdx (arg3), r10 (arg4), r8 (arg5), r9 (arg6)
-
-@ all asm programs starts from the label _start
 _start:
         call _printHello
         call _exit
