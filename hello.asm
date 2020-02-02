4e919deb9422fa9ebf6783f4e6a896623bd47617
Nicolas Guillaume <nicolas.f.guillaume@gmail.com>
2020-02-02 23:09:23 +0100

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -1,3 +1,11 @@
+@ define constants
+STDIN equ 0
+STDOUT equ 1
+STDERR equ 2
+SYS_READ equ 0
+SYS_WRITE equ 1
+SYS_EXIT equ 60
+
 section .data
         @ db means define bytes (each character is a byte)
         @ my_text is a pointer to a memory address that contains the data
@@ -13,18 +21,18 @@ _start:
         
 @ subroutine to create a system call: sys_write(1, my_text, 14)
 _printHello:
-        mov rax, 1          @ change value of rax register: 1 (ID: sys_write)
-        mov rdi, 1          @ change value of rdi register: 1 (arg1: file descriptor: standard output)
-        mov rsi, my_text    @ change value of rsi register: my_text (arg2: buffer: location of string to write)
-        mov rdx, 14         @ change value of rdx register: 14 (arg3: count: length of string)
+        mov rax, SYS_WRITE       @ update value of rax register: 1 (ID: sys_write)
+        mov rdi, STDOUT          @ update value of rdi register: 1 (arg1: file descriptor: standard output)
+        mov rsi, my_text         @ update value of rsi register: my_text (arg2: buffer: location of string to write)
+        mov rdx, 14              @ update value of rdx register: 14 (arg3: count: length of string)
         syscall
         
         ret
  
  @ subroutine to create a system call: sys_exit(0)
 _exit:
-        mov rax, 60         @ change value of rax register: 60 (ID: sys_exit)
-        mov rdi, 0          @ change value of rdi register: 0 (arg1: error code, 0 if no error)
+        mov rax, SYS_EXIT        @ update value of rax register: 60 (ID: sys_exit)
+        mov rdi, 0               @ update value of rdi register: 0 (arg1: error code, 0 if no error)
         syscall
 
         ret
